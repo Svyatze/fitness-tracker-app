b428d6044737364a599b7158638c2e6674fa9ebc
Svyatoslav Kondratchuk <konrsvyat@gmail.com>
2023-09-25 11:13:24 +0300

    Adjusted training UI and logic.

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from "@angular/material/dialog";
 import { StopTrainingComponent } from "./stop-training/stop-training.component";
+import { TrainingService } from "../training.service";
 
 @Component({
   selector: 'app-current-training',
@@ -8,13 +9,12 @@ import { StopTrainingComponent } from "./stop-training/stop-training.component";
   styleUrls: ['./current-training.component.scss']
 })
 export class CurrentTrainingComponent implements OnInit {
-  @Output() trainingExit = new EventEmitter<any>();
   progress = 0;
   //@ts-ignore
   timer: number;
 
 
-  constructor(private dialog: MatDialog) {
+  constructor(private dialog: MatDialog, private trainingService: TrainingService) {
   }
 
   ngOnInit() {
@@ -22,12 +22,14 @@ export class CurrentTrainingComponent implements OnInit {
   }
 
   startOrResumeTimer() {
+    const step = this.trainingService.getRunningExercise().duration! / 100 * 1000;
     this.timer = setInterval(() => {
-      this.progress = this.progress + 5;
+      this.progress = this.progress + 1;
       if (this.progress >= 100) {
+        this.trainingService.completeExercise();
         clearInterval(this.timer)
       }
-    }, 1000);
+    }, step);
   }
 
   onStop() {
@@ -40,7 +42,7 @@ export class CurrentTrainingComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe(result => {
       if (result) {
-        this.trainingExit.emit();
+        this.trainingService.cancelExercise(this.progress)
       } else {
         this.startOrResumeTimer();
       }
--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,17 +1,25 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TrainingService } from "../training.service";
+import { Exercise } from "../exercise.model";
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-new-training',
   templateUrl: './new-training.component.html',
   styleUrls: ['./new-training.component.scss']
 })
-export class NewTrainingComponent {
-  @Output() trainingStart = new EventEmitter<void>()
+export class NewTrainingComponent implements OnInit {
+  exercises: Exercise[] = [];
 
-  constructor() {
+  constructor(private trainingService: TrainingService) {
   }
 
-  onStartTraining() {
-    this.trainingStart.emit();
+  ngOnInit() {
+    this.exercises = this.trainingService.getAvailableExercises();
   }
+
+  onStartTraining(form: NgForm) {
+    this.trainingService.startExercise(form.value.exercise);
+  }
+
 }
