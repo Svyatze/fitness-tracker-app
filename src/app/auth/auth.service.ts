5402e6e45f113a424f9ffd37c2e1fbe00d01a7e0
Svyatoslav Kondratchuk <konrsvyat@gmail.com>
2023-11-10 16:27:18 +0200

    Added AngularFire auth.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,51 +1,56 @@
-import { Injectable } from "@angular/core";
-import { User } from "./user.model";
-import { AuthData } from "./auth-data.model";
-import { Subject } from "rxjs";
-import { Router } from "@angular/router";
+import {Injectable} from "@angular/core";
+import {User} from "./user.model";
+import {AuthData} from "./auth-data.model";
+import {Subject} from "rxjs";
+import {Router} from "@angular/router";
+import {AngularFireAuth} from "@angular/fire/compat/auth";
 
 @Injectable()
 export class AuthService {
   authChange = new Subject<boolean>();
-  private user: User | null = null;
+  private isAuthenticated = false;
 
-  constructor(private router: Router) {
+  constructor(
+    private router: Router,
+    private afAuth: AngularFireAuth) {
   }
 
 
   registerUser(authData: AuthData) {
-    this.user = {
-      email: authData.email,
-      userId: Math.round(Math.random() * 10000).toString()
-    }
+    this.afAuth.createUserWithEmailAndPassword(
+      authData.email,
+      authData.password
+    ).then(result => {
+      this.authSuccessfully();
+    }).catch(error =>
+      console.error(error));
     this.authChange.next(true);
-    this.authSuccessfully();
   }
 
   login(authData: AuthData) {
-    this.user = {
-      email: authData.email,
-      userId: Math.round(Math.random() * 10000).toString()
-    }
-    this.authChange.next(true);
-    this.authSuccessfully();
+    this.afAuth.signInWithEmailAndPassword(
+      authData.email,
+      authData.password
+    ).then(result => {
+      console.log(result);
+      this.authSuccessfully();
+    }).catch(error =>
+      console.error(error));
   }
 
   logout() {
-    this.user = null;
     this.authChange.next(false);
-    this.router.navigate(['/login'])
+    this.router.navigate(['/login']);
+    this.isAuthenticated = false;
   }
 
-  getUser() {
-    return structuredClone(this.user)
-  }
 
   isAuth() {
-    return this.user != null;
+    return this.isAuthenticated;
   }
 
   private authSuccessfully() {
+    this.isAuthenticated = true;
     this.authChange.next(true);
     this.router.navigate(['/training']);
   }
